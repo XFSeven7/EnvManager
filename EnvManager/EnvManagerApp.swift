c03f2f56d92da4796927761f4ffcebc761cac014
kuafu <kuafu@u1s1.biz>
2025-05-26 16:29:37 +0400

    Initial Commit

---
+++ EnvManager/EnvManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvManagerApp.swift
+//  EnvManager
+//
+//  Created by kuafu on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct EnvManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
